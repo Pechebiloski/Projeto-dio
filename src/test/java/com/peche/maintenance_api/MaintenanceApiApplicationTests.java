f86408f9d4d1cd19c1fbe2ec8cdbbf29a6dbeb19
Pechebiloski <devpechebiloski@gmail.com>
2026-09-11 15:52:09 -0300

    chore: initial project setup

---
+++ src/test/java/com/peche/maintenance_api/MaintenanceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peche.maintenance_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenanceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
